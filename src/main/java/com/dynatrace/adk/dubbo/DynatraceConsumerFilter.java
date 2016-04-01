7d359bc38e5e8c59ccc211010eb6f71cfc852ae3
Wei Ding <deanwade@126.com>
2016-04-01 18:14:15 +0800

    Improve to code to make it more production suitable

--- src/main/java/com/dynatrace/adk/dubbo/DynatraceConsumerFilter.java
+++ src/main/java/com/dynatrace/adk/dubbo/DynatraceConsumerFilter.java
@@ -11,44 +11,44 @@ import com.alibaba.dubbo.rpc.support.RpcUtils;
 import com.dynatrace.adk.DynaTraceADKFactory;
 import com.dynatrace.adk.Tagging;
 
-@Activate(group = Constants.CONSUMER, order=Integer.MAX_VALUE)
+@Activate(group = Constants.CONSUMER, order = Integer.MAX_VALUE)
 public class DynatraceConsumerFilter implements Filter {
-	
+
 	private static final String DYNATRACE_TAG_KEY = "DYNATRACE_TAG_KEY";
-	
+
 	public DynatraceConsumerFilter() {
 		DynaTraceADKFactory.class.getName();
 	}
 
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-			
-		// initialize the dynaTrace ADK
-		DynaTraceADKFactory.initialize();
-		
-		// get an instance of the Tagging ADK
-		Tagging tagging = DynaTraceADKFactory.createTagging();
-		
-		String tagString = tagging.getTagAsString();
-		
-		recordTagString(tagString);
-					
-		boolean isAsync = RpcUtils.isAsync(invoker.getUrl(), invocation);
-		
-		tagging.linkClientPurePath(isAsync, tagString);
-		
-		invocation.getAttachments().put(DYNATRACE_TAG_KEY, tagString);
-		
+
+		try {
+			// initialize the dynaTrace ADK
+			DynaTraceADKFactory.initialize();
+
+			// get an instance of the Tagging ADK
+			Tagging tagging = DynaTraceADKFactory.createTagging();
+
+			String tagString = tagging.getTagAsString();
+
+			boolean isAsync = RpcUtils.isAsync(invoker.getUrl(), invocation);
+
+			tagging.linkClientPurePath(isAsync, tagString);
+
+			invocation.getAttachments().put(DYNATRACE_TAG_KEY, tagString);
+		} catch (Throwable t) {
+			// do nothing
+		}
+
 		Result result = invoker.invoke(invocation);
-		
-		DynaTraceADKFactory.uninitialize();
-		
-		return result;		
-	}
-	
-	//this is a empty method for dynaTrace to monitor the argument
-	private void recordTagString(String tagString){
-		//System.out.println("invoke in " + this.getClass().getName() + "wit tagString: " + tagString);
-		return;
+
+		try {
+			DynaTraceADKFactory.uninitialize();
+		} catch (Throwable t) {
+			// do nothing
+		}
+
+		return result;
 	}
 
 }
--- src/main/java/com/dynatrace/adk/dubbo/DynatraceProviderFilter.java
+++ src/main/java/com/dynatrace/adk/dubbo/DynatraceProviderFilter.java
@@ -22,35 +22,38 @@ public class DynatraceProviderFilter implements Filter {
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
 		
 		String tagString = invocation.getAttachments().get(DYNATRACE_TAG_KEY);
-		if(tagString != null){// this is a consumer
-			
-			recordTagString(tagString);
-			
-			// initialize the dynaTrace ADK
-			DynaTraceADKFactory.initialize();
-			
-			// get an instance of the Tagging ADK
-			Tagging tagging = DynaTraceADKFactory.createTagging();
-			
-			tagging.setTagFromString(tagString);
-			
-			tagging.startServerPurePath();
-			
+		
+		if(tagString != null){
+			Tagging tagging = null;
+			try {
+				// initialize the dynaTrace ADK
+				DynaTraceADKFactory.initialize();
+				
+				// get an instance of the Tagging ADK
+				tagging = DynaTraceADKFactory.createTagging();
+				
+				tagging.setTagFromString(tagString);
+				
+				tagging.startServerPurePath();
+			} catch (Throwable t) {
+				// do nothing
+			}
+					
 			Result result = invoker.invoke(invocation);
 			
-			tagging.endServerPurePath();
+			try {
+				if(tagging != null){
+					tagging.endServerPurePath();
+				}
+			} catch (Throwable t) {
+				// do nothing
+			}
 			
 			return result;
 		}else{
 			return invoker.invoke(invocation);
 		}
 		
-	}
-	
-	//this is a empty method for dynaTrace to monitor the argument
-	private void recordTagString(String tagString){
-		//System.out.println("invoke in " + this.getClass().getName() + "wit tagString: " + tagString);
-		return;
 	}
 
 }
